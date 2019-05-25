eed7bd587e716c8adab7bd7f01fadbfaadbed841
kwikl3arn <44991804+kwikl3arn@users.noreply.github.com>
2019-05-25 14:22:15 +0530

    Create model.php

---
+++ model.php
@@ -0,0 +1,24 @@
+class Demo_Model extends CI_Model {
+
+    public function __construct() {
+        parent::__construct();
+        error_reporting(0);
+    }
+     //user login check
+    public function userchk($id, $pass) {
+        $this->db->select("*");
+        $this->db->from("users");
+        $this->db->where("user_id", $id);
+        $this->db->where("user_password", $pass);
+        $query = $this->db->get();
+
+        if ($query->num_rows() > 0) {
+            $session_data = $query->row_array();
+            $this->session->set_userdata('sessiondata', $session_data);
+                    return 1;
+        } else {
+            return 0;
+        }
+    }
+    
+    }
