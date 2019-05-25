89762bbb89aecaa99ab912f2fc077a5c84ea5505
kwikl3arn <44991804+kwikl3arn@users.noreply.github.com>
2019-05-25 14:29:53 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+$ServerPath = $_SERVER['DOCUMENT_ROOT'];
+$scriptname = explode('/', $_SERVER['SCRIPT_NAME']);
+if ((isset($_SERVER['HTTPS']) && (strtolower($_SERVER['HTTPS'])) !== 'off') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) {
+    $WebPath = 'https://';
+} else {
+    $WebPath = 'http://';
+}
+$WebPath .= isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost';
+$BaseURL = $WebPath; //. '/' . $scriptname[1];
+$EBASEURL = $WebPath; //. '/' . $scriptname[1];
+for ($i = 1; $i < count($scriptname) - 1; $i++) {
+    $BaseURL .= '/' . $scriptname[$i];
+}
+$config['base_url'] = $BaseURL;
